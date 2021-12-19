0cebb82f6fb79a8fcb8bd10092f64cbc849aab26
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-12-19 16:11:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var config = {
 
 var player;
 var breads;
-var traps;
+var bombs;
 var platforms;
 var cursors;
 var enemies;
@@ -27,16 +27,9 @@ var gameOver = false;
 var scoreText;
 var cityTile;
 var enemyData = [
-  { x: 75, y: 50 },
-  { x: 150, y: 0 },
-  { x: 250, y: 250 },
-  { x: 275, y: 0 },
-  { x: 350, y: 100 },
-  { x: 450, y: 300 },
-  { x: 475, y: 0 },
-  { x: 525, y: 75 },
-  { x: 650, y: 0 },
-  { x: 700, y: 400 },
+  { x: 770, y: 220 },
+  { x: 550, y: 250 },
+  { x: 450, y: 400 }
 ];
 var game = new Phaser.Game(config);
 
@@ -110,37 +103,34 @@ function create() {
     frameRate: 10,
     repeat: -1,
   });
-  // enemies = this.physics.add.group({
-  //     key: 'snake',
-  //     repeat: 3,
-  //     setXY: {x: Math.random() * 250+200, y: 650, stepX: Phaser.Math.random() * 250}
-  // });
+  this.anims.create({
+    key: "Sleft",
+    frames: this.anims.generateFrameNumbers("snake", { start: 0, end: 7 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+  this.anims.create({
+    key: "Sturn",
+    frames: [{ key: "snake", frame: 8 }],
+    frameRate: 20,
+  });
+  this.anims.create({
+    key: "Sright",
+    frames: this.anims.generateFrameNumbers("snake", { start: 9, end: 16 }),
+    frameRate: 10,
+    repeat: -1,
+  });
   enemies = this.physics.add.group();
-  enemies.create(300, 650, "snake");
   for (var i = 0; i < enemyData.length; i++) {
-    var enemy = enemies.create(enemyData[i].x, enemyData[i].y, "snake");
-    enemy.body.velocity.x = Phaser.Math.Between(125, 175);
-    this.anims.create({
-      frames: this.anims.generateFrameNumbers("snake", { start: 0, end: 7 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.anims.create({
-      frames: [{ key: "snake", frame: 8 }],
-      frameRate: 20,
-    });
-    this.anims.create({
-      frames: this.anims.generateFrameNumbers("snake", { start: 9, end: 16 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    // enemyData[i].anchor.setTo(0.5, 0.5);
-    // enemyData[i].animations.add('moving', [0, 1, 2], 10, true);
-    // enemyData[i].animations.play('moving');
-    // enemyData[i].body.velocity.y = 50;
+    enemies.create(enemyData[i].x, enemyData[i].y, "snake");
   }
-  // enemies.setBounce(0);
-  // enemies.setCollideWorldBounds(true);
+
+  enemies.getChildren().forEach(function(enemy){
+		enemy.body.velocity.x = Phaser.Math.Between(20, 70);
+    enemy.stepCount = Phaser.Math.Between(10, 100);
+    enemy.normalVelocity = enemy.body.velocity.x;
+  }, this);
+
   //  Input Events
   cursors = this.input.keyboard.createCursorKeys();
 
@@ -169,17 +159,14 @@ function create() {
   this.physics.add.collider(enemies, platforms);
   this.physics.add.collider(breads, platforms);
   this.physics.add.collider(bombs, platforms);
-  //this.physics.add.collider(player, traps);
   this.physics.add.collider(breads, traps);
-  this.physics.add.collider(bombs, traps);
+  this.physics.add.collider(enemies, traps);
 
   this.physics.add.overlap(player, breads, collectbread, null, this);
 
   this.physics.add.collider(player, enemies, hitTrap, null, this);
-  this.physics.add.collider(player, traps, hitTrap, null, this);
-  //collider dla wrogow wchodzacych na pulapki
-  //this.physics.add.collider(enemies, traps, hitTrap, null, this);
-  //cityTile = game.add.tilesprite(0, 0, 960, 375, 'city')
+  this.physics.add.collider(player, traps, hitTrap, null, this);  
+  this.physics.add.collider(enemies, traps, hitTrap, null, this);
 }
 
 function update() {
@@ -203,8 +190,11 @@ function update() {
   if (cursors.up.isDown && player.body.touching.down) {
     player.setVelocityY(-350);
   }
-  //snakeBehaviour()
-}
+
+  enemies.getChildren().forEach(function (enemy) {    
+    snakeBehaviour(enemy)
+    patrolPlatform(enemy)
+});}
 
 function collectbread(player, bread) {
   bread.disableBody(true, true);
@@ -213,25 +203,16 @@ function collectbread(player, bread) {
   score += 10;
   scoreText.setText("Score: " + score);
 
-  if (breads.countActive(true) === 0) {
+  //if (breads.countActive(true) === 0) {
+    if (breads.countActive(true) < 3) {
     //  A new batch of breads to collect
     breads.children.iterate(function (child) {
-      child.enableBody(true, child.x, 0, true, true);
+      child.enableBody(true, player.x + Math.random() * 600 + 300, 0, true, true);
     });
 
-    var x =
-      player.x < 400
-        ? Phaser.Math.Between(400, 800)
-        : Phaser.Math.Between(0, 400);
-
-    var bomb = bombs.create(x, 16, "bomb");
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    bomb.allowGravity = false;
+    generateEnemy()
   }
 }
-
 function hitTrap(player, traps) {
   this.physics.pause();
 
@@ -241,8 +222,6 @@ function hitTrap(player, traps) {
 
   gameOver = true;
 }
-
-//zabijanie wrogów na trapach
 function hitTrapEnemy(enemy, traps) {
   this.physics.pause();
 
@@ -252,36 +231,84 @@ function hitTrapEnemy(enemy, traps) {
 
   gameOver = true;
 }
+function hitBomb(player, bomb) {
+  this.physics.pause();
 
-// let stepLimit = 100;
-// function snakeBehaviour()
-// {
-//     // see if enemy and player within 400px of each other
-// if (game.physics.arcade.distanceBetween(enemy, player) < 400) {
+  player.setTint(0xff0000);
 
-//     // if player to left of enemy AND enemy moving to right (or not moving)
-//     if (player.x < enemy.x && enemy.body.velocity.x >= 0) {
-//         // move enemy to left
-//         enemy.body.velocity.x = -150;
-//     }
-//     // if player to right of enemy AND enemy moving to left (or not moving)
-//     else if (player.x > enemy.x && enemy.body.velocity.x <= 0) {
-//         // move enemy to right
-//         enemy.body.velocity.x = 150;
-//     }
-// }
-// function patrolPlatform(enemy, platform) {
-//     // if enemy moving to right and has started to move over right edge of platform
-//     if (enemy.body.velocity.x > 0 && enemy.right > platform.right) {
-//         enemy.body.velocity.x *= -1; // reverse direction
-//     }
-//     // else if enemy moving to left and has started to move over left edge of platform
-//     else if (enemy.body.velocity.x < 0 && enemy.left < platform.left) {
-//         enemy.body.velocity.x *= -1; // reverse direction
-//     }
-// }
-// function generateEnemy () {
-//     const xCoordinate = Math.random() * 250+200;
-//     enemies.create(xCoordinate, 650, 'enemy');
-//   }
-// }
+  player.anims.play("turn");
+
+  gameOver = true;
+}
+ let stepLimit = 100;
+function snakeBehaviour(enemy)
+{
+  enemy.stepCount++;
+    // check if enemy's step counter has reach limit
+    if (enemy.stepCount > stepLimit) {
+        // reverse enemy direction
+        enemy.body.velocity.x *= -1;
+        // reset enemy's step counter
+        enemy.stepCount = 0;
+        // can add other code - change enemy animation, etc.
+    }
+    if(enemy.body.velocity.x < 0) enemy.anims.play("Sleft", true);
+    if(enemy.body.velocity.x > 0) enemy.anims.play("Sright", true);
+    if(enemy.body.velocity.x == 0) enemy.anims.play("Sstop", true);
+    // see if enemy and player within 400px of each other
+if (Math.abs(player.x - enemy.x) < 300 && player.y === enemy.y) {
+
+    // if player to left of enemy AND enemy moving to right (or not moving)
+    if (player.x < enemy.x && enemy.body.velocity.x >= 0) {
+        // move enemy to left
+        enemy.body.velocity.x *= -1;
+    }
+    // if player to right of enemy AND enemy moving to left (or not moving)
+    else if (player.x > enemy.x && enemy.body.velocity.x <= 0) {
+        // move enemy to right
+        enemy.body.velocity.x *= -1;
+    }
+     if(enemy.body.velocity.x <= 50 && enemy.body.velocity.x >=0 ) {enemy.body.velocity.x *= 3}
+    else if(enemy.body.velocity.x >= -50 && enemy.body.velocity.x <=0 ) {enemy.body.velocity.x *= 3}
+
+}
+if (Math.abs(player.x - enemy.x) > 300) {
+
+  // if player to left of enemy AND enemy moving to right (or not moving)
+  if (enemy.body.velocity.x > 0) {
+      // move enemy to left
+      enemy.body.velocity.x = enemy.normalVelocity;
+  }
+  // if player to right of enemy AND enemy moving to left (or not moving)
+  else if (enemy.body.velocity.x < 0) {
+      // move enemy to right
+      enemy.body.velocity.x = - enemy.normalVelocity;
+  }
+}
+}//NIE DZIALA HEHE
+function patrolPlatform(enemy) {
+  platforms.getChildren().forEach(platform => {
+        // if enemy moving to right and has started to move over right edge of platform
+        if (enemy.body.velocity.x > 0 && enemy.x.right < platform.x.right) {
+          debugger
+    
+            enemy.body.velocity.x *= -1; // reverse direction
+        }
+        // else if enemy moving to left and has started to move over left edge of platform
+        else if (enemy.body.velocity.x < 0 && enemy.x.left < platform.x.left) {
+          debugger
+    
+            enemy.body.velocity.x *= -1; // reverse direction
+        }
+  });
+
+}
+function generateEnemy () {
+
+  for (let i = 0; i < 3; i++) {
+    enemies.create(player.x + Math.random() * 600 + 300, Phaser.Math.Between(200, 600), 'snake');
+    enemies.getChildren()[enemies.getChildren().length-1].body.velocity.x = Phaser.Math.Between(20, 70);
+    enemies.getChildren()[enemies.getChildren().length-1].stepCount = Phaser.Math.Between(10, 100);
+    enemies.getChildren()[enemies.getChildren().length-1].normalVelocity = enemies.getChildren()[enemies.getChildren().length-1].body.velocity.x;
+  }
+}
